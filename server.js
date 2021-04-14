1060f468eaef487f29705fcdb29427a4cb605f96
Salo <salomonco@gmail.com>
2021-04-14 08:17:27 -0500

    started TESTS

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
-
-import 'dotenv/config';
-import { addHook } from 'pirates';
-
-
 // App Setup
 const app = express();
 const port = 3000
@@ -27,10 +22,8 @@ require('./controllers/posts.js')(app);
 // Set db
 require('./data/reddit-db');
 
-
-// Routes
-
-
 app.listen(port, () => {
   console.log('Search listening on port localhost:3000!');
 });
+
+module.exports = app;
