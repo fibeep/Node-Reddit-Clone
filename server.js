6cd008ea7f83821fcb5a24d49ea51c4f2f5bf536
Salo <salomonco@gmail.com>
2021-04-27 21:15:50 -0500

    implemented nested comments

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.set('view engine', 'handlebars');
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
+require("./controllers/replies.js")(app);
 
 // Set db
 require('./data/reddit-db');
