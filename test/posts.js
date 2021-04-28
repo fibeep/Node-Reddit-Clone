a23b104eee2cefe07055c20bc0f70b25684db137
Salo <salomonco@gmail.com>
2021-04-27 21:00:48 -0500

    fixed test bugs

--- test/posts.js
+++ test/posts.js
@@ -19,7 +19,8 @@ describe('Posts', function() {
   const newPost = {
       title: 'post title',
       url: 'https://www.google.com',
-      summary: 'post summary'
+      summary: 'post summary',
+      subreddit: 'testsubreddit'
   };
   const user = {
     username: "poststest",
