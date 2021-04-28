f1e2f2481b9cd82fb1eb0c25342b284799d737d5
Salo <salomonco@gmail.com>
2021-04-27 20:59:13 -0500

    fixed posts test

--- test/auth.js
+++ test/auth.js
@@ -4,6 +4,7 @@ const server = require("../server");
 const should = chai.should();
 chai.use(chaiHttp);
 
+
 // Agent that will keep track of our cookies
 const agent = chai.request.agent(server);
 
--- test/posts.js
+++ test/posts.js
@@ -3,12 +3,13 @@ const app = require("./../server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
+const agent = chai.request.agent(app);
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post');
 const server = require('../server');
-
+const User = require("../models/user");
 chai.should();
 chai.use(chaiHttp);
 
@@ -20,6 +21,22 @@ describe('Posts', function() {
       url: 'https://www.google.com',
       summary: 'post summary'
   };
+  const user = {
+    username: "poststest",
+    password: "testposts",
+  };
+  before(function (done) {
+    agent
+      .post("/sign-up")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(user)
+      .then(function (res) {
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
   it("should create with valid attributes at POST /posts/new", function (done) {
     //Checks how many posts there are now
     Post.estimatedDocumentCount()
@@ -52,7 +69,23 @@ describe('Posts', function() {
             done(err)
         });
   });
-  after(function () {
-      Post.findOneAndDelete(newPost);
+  after(function (done) {
+    Post.findOneAndDelete(newPost)
+      .then(function (res) {
+        agent.close();
+
+        User.findOneAndDelete({
+          username: user.username,
+        })
+          .then(function (res) {
+            done();
+          })
+          .catch(function (err) {
+            done(err);
+          });
+      })
+      .catch(function (err) {
+        done(err);
+      });
   });
 });
